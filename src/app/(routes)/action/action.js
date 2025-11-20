5793d5942e27e1f242dd5490d2272152a6af5bad
My <idla0003@stud.kea.dk>
2025-11-20 11:04:04 +0100

    fix: improve error handling for product name validation in submitProduct function

--- src/app/(routes)/action/action.js
+++ src/app/(routes)/action/action.js
@@ -4,9 +4,12 @@ export const submitProduct = async (prevState, formData) => {
   const error = {};
   const productName = formData.get("productname");
 
-//   Hvis productname ikke er udfyldt (man submitter et tomt felt) skal der komme en errorbesked, som du ser herunder:
-  if(!productName){
+  //   Hvis productname ikke er udfyldt (man submitter et tomt felt) skal der komme en errorbesked, som du ser herunder:
+  if (!productName) {
     error.productName = "Feltet skal udfyldes!";
+  } else if (productName.length < 5) {
+    error.productName = "Produktnavnet skal være mindst 5 tegn langt!";
   }
+
   return { error, productName };
 };
