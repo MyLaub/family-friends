f83df788c0ef34b15132e4ba51db054b86e52bbd
Frida Lindbjerg <fridalindbjerg@outlook.dk>
2025-11-21 09:19:19 +0100

    fix: enhance product submission error handling for price validation and update success message display

--- src/app/(routes)/action/action.js
+++ src/app/(routes)/action/action.js
@@ -3,16 +3,22 @@
 export const submitProduct = async (prevState, formData) => {
   const error = {};
   const productName = formData.get("productname");
+  const productPrice = formData.get("productprice");
 
   //   Hvis productname ikke er udfyldt (man submitter et tomt felt) skal der komme en errorbesked, som du ser herunder:
   if (!productName) {
     error.productName = "Feltet skal udfyldes!";
   } else if (productName.length < 5) {
     error.productName = "Produktnavnet skal være mindst 5 tegn langt!";
-  } 
+  }
+  if (!productPrice) {
+    error.productPrice = "Feltet skal udfyldes!";
+  } else if (isNaN(Number(productPrice))) {
+    error.productPrice = "indtast gyldig pris!";
+  }
 
-  if (Object.keys(error).lenght > 0) {
-    return {error, productName};
+  if (Object.keys(error).length > 0) {
+    return { error, productName, productPrice };
   }
 
   return { succes: true };
--- src/app/(routes)/addproduct/page.js
+++ src/app/(routes)/addproduct/page.js
@@ -7,30 +7,22 @@ const addProduct = () => {
   console.log(state);
   return (
     <div>
-    
-        {state.succes &&(
-            <p className="text-green-600">Tillykke din formular er sendt!</p>)}
+      {state.succes && <p className="text-green-600">Tillykke din formular er sendt!</p>}
 
-    <form action={postProduct}>
-
+      <form action={postProduct}>
         {/* Synlig fejlbesked: */}
 
-        {state.error?.productName && 
-        <p className="text-red-600">{state.error.productName}</p>}
+        {state.error?.productName && <p className="text-red-600">{state.error.productName}</p>}
 
-      <input 
-      type="text" 
-      name="productname" 
-      placeholder="Product name..." 
-      className="rounded-lg border border-gray-300 px-3 py-2 "
-      defaultValue={state.productName}>
-      </input>
-      <button 
-      type="submit" 
-      className="justify-center items-center px-4 py-2 rounded-lg text-white bg-blue-600 ">
-        Submit product
-      </button>
-    </form>
+        <input type="text" name="productname" placeholder="Product name..." className="rounded-lg border border-gray-300 px-3 py-2 " defaultValue={state.productName}></input>
+
+        {state.error?.productPrice && <p className="text-red-600">{state.error.productPrice}</p>}
+
+        <input name="productprice" placeholder="Product price..." className="rounded-lg border border-gray-300 px-3 py-2 " defaultValue={state.productPrice}></input>
+        <button type="submit" className="justify-center items-center px-4 py-2 rounded-lg text-white bg-blue-600 ">
+          Submit product
+        </button>
+      </form>
     </div>
   );
 };
